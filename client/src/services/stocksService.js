a74a0826c366516f5ed83f0be716bda2cb9e793e
Lucas Aquiles <lucasaquiles@gmail.com>
2020-05-05 23:08:37 -0300

    boilerplate

---
+++ client/src/services/stocksService.js
@@ -0,0 +1,7 @@
+import Api from './Api'
+
+export default {
+    fetchStocks () {
+        return Api().get('stocks')
+    }
+}
---
+++ server/src/app.js
@@ -0,0 +1,30 @@
+const express = require("express")
+const cors = require("cors")
+const morgan = require("morgan")
+
+const app = express() 
+app.use(morgan('combined'))
+app.use(express.json())
+app.use(cors())
+
+app.get('/stocks', (req, res) => {
+
+    res.send(
+        [
+            {
+                code: "IBOV",
+                amount: 6
+            },
+            {
+                code: "XPCAGEY",
+                amount: 2
+            },
+            {
+                code: "XPCAGEY2",
+                amount: 2
+            }
+        ]
+    )
+})
+
+app.listen(process.env.PORT || 8081)
