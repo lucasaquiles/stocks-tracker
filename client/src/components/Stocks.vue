b4a156b568a3d727dc676edf86d9966aeb231d9c
Lucas Aquiles <lucasaquiles@gmail.com>
2020-05-05 23:20:27 -0300

    binding vue and express

---
+++ client/src/components/Stocks.vue
@@ -0,0 +1,15 @@
+<template>
+
+    <div class="stocks">
+        All stocks 
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'stocks',
+        data () {
+            return {}
+        }
+    }
+</script>
