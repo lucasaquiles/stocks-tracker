9a66443e6e03cb61f22ff3b61791f3d258a34725
Lucas Aquiles <lucasaquiles@gmail.com>
2020-07-19 20:45:28 -0300

    lista de rendimentos nos ultimos 12 meses

--- server/src/routers/stocks/stockService.js
+++ server/src/routers/stocks/stockService.js
@@ -6,23 +6,53 @@ exports.listAll = async function () {
     return stocks;
 }
 
+function parseFloat(value) {
+    return Number.parseFloat(value.trim().replace("R$", "").replace(",", ".").trim());
+}
+
+function parseStringToFloatPercentage(value) {
+    return Number.parseFloat(value.trim().replace("%$", "").replace(",", ".").trim());
+}
+
 exports.extract =  function (response) {
 
     const html = response;
     const $ =  cheerio.load(html);
-
     const base = $("#informations--indexes .item").text().split("\n");
-
+    
     const cotacaoWrapperInfo = $("#quotations--infos-wrapper");
     const valorCotacao = cotacaoWrapperInfo.find($('.quotation')).text().split("\n");
     
-    const yeldValue = Number.parseFloat(base[1].trim().replace("R$", "").replace(",", ".").trim());
-    const dividendos = Number.parseFloat(base[4].trim().replace("R$", "").replace(",", ".").trim());
-    const stockValue = Number.parseFloat(valorCotacao[2].trim().replace(",","."));
+    const dividendLast12Months = findRendimentosUltimos12Meses($)
+
+    const yeldValue = parseFloat(base[1].trim().replace("R$", "").replace(",", ".").trim());
+    const dividendos = parseFloat(base[4].trim().replace("R$", "").replace(",", ".").trim());
+    const stockValue = parseFloat(valorCotacao[2].trim().replace(",","."));
 
     return {
         dividendYeld: yeldValue,
         lastDividend: dividendos,
-        value: stockValue
+        value: stockValue,
+        dividendLast12Months: dividendLast12Months
     };
+}
+
+function findRendimentosUltimos12Meses($) {
+
+    const tableRendimentosBase = $("#last-revenues--table tbody tr");
+    const dividendLast12Months = [];
+
+    tableRendimentosBase.each((index, element) => {
+        const splitedElement = $(element).find("td");
+        const item = {
+            base: $(splitedElement.get(0)).text(),
+            paymentDate: $(splitedElement.get(1)).text(),
+            stockValue: parseFloat($(splitedElement.get(2)).text()),
+            yeldPercentage: parseStringToFloatPercentage($(splitedElement.get(3)).text()),
+            rendimento: parseFloat($(splitedElement.get(4)).text())
+        };
+        dividendLast12Months.push(item);
+    });
+
+    return dividendLast12Months
 }
